045d2e5ab26dbfc1d8457bc13b27cc4870ce008d
Bryan Plant <bryanplant@gmail.com>
2018-04-09 11:48:31 -0600

    adds mybot

--- src/main/java/csci305/javalab/MyBot.java
+++ src/main/java/csci305/javalab/MyBot.java
@@ -3,19 +3,58 @@ package csci305.javalab;
 import java.util.Map;
 import java.util.Random;
 
-public class MyBot extends Player{
+public class MyBot extends Player {
 
 
-    public MyBot(String name, Map<String, Element> moves) {
+    MyBot(String name, Map<String, Element> moves) {
         super(name, moves);
     }
 
     @Override
     public Element play() {
-        if(opponent.lastPlay == null)
+        if (opponent.lastPlay == null)
             currentPlay = new RandomBot("Random Bot", moves).play();
         else {
-            currentPlay = opponent.lastPlay;
+            Random random = new Random();
+            int num = random.nextInt(2);
+
+            if (num == 0) {
+                switch (opponent.lastPlay.getName()) {
+                    case "Rock":
+                        currentPlay = moves.get("Paper");
+                        break;
+                    case "Paper":
+                        currentPlay = moves.get("Scissors");
+                        break;
+                    case "Scissors":
+                        currentPlay = moves.get("Spock");
+                        break;
+                    case "Lizard":
+                        currentPlay = moves.get("Rock");
+                        break;
+                    case "Spock":
+                        currentPlay = moves.get("Lizard");
+                        break;
+                }
+            } else {
+                switch (opponent.lastPlay.getName()) {
+                    case "Rock":
+                        currentPlay = moves.get("Spock");
+                        break;
+                    case "Paper":
+                        currentPlay = moves.get("Lizard");
+                        break;
+                    case "Scissors":
+                        currentPlay = moves.get("Rock");
+                        break;
+                    case "Lizard":
+                        currentPlay = moves.get("Scissors");
+                        break;
+                    case "Spock":
+                        currentPlay = moves.get("Paper");
+                        break;
+                }
+            }
         }
 
         return currentPlay;
